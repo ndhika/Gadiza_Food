1a1d6a8cb4270b82751060a5af6f466527e37a41
ndhika <azumarafi@gmail.com>
2024-03-28 23:54:37 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('/login/login');
+});
+
+Route::get('/about', function () {
+    return view('/about/about');
+});
+
+Route::get('/menu', function () {
+    return view('/menu/menu');
+});
+
+Route::get('/profile', function () {
+    return view('/profile/profile');
+});
+
+Route::get('/keranjang', function () {
+    return view('/keranjang/keranjang');
 });
