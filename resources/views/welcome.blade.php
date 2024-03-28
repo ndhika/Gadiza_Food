40a1edd1a1a9bce39b00236801c60bcb6e043ce5
ndhika <azumarafi@gmail.com>
2024-03-28 22:34:58 +0700

    '#'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,11 @@
         <title>Gadiza Food</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+     
         <!-- Styles -->
-        <style>
-
-        </style>
+        
     </head>
-    <body class="antialiased">
+    <body>
+        
     </body>
 </html>
